db62ca15c07efdf3365b84eb6e4bdb5604ee6c48
Deniz Berk Bozkurt <74145056+kolari53@users.noreply.github.com>
2022-04-11 12:08:08 +0300

    Adding ChatbotApp

---
+++ ChatbotWithSpring/src/main/java/com/chatbotJava/ChatbotWithSpring/ChatbotWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.chatbotJava.ChatbotWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotWithSpringApplication.class, args);
+	}
+
+}
---
+++ ChatbotWithSpring/src/test/java/com/chatbotJava/ChatbotWithSpring/ChatbotWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbotJava.ChatbotWithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
